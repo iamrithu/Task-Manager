58850b0bbc23e5ad5c46ae3f4da628fbab957b00
iamrithu <ritimahesh29@gmail.com>
2022-05-08 22:31:43 +0530

    --Added Model

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+require("dotenv").config();
+
 const express = require("express");
-
 const app = express();
-
 const port = 3000;
-
 const tasks = require("./routes/task");
+const connectDB = require("./db/connect");
 
-//middleware
+//middleware...
 
 app.use(express.json());
 
-//routing
+//routing...
 
 app.use("/api/v1/tasks", tasks);
 
-//listening
+//listening...
 
-app.listen(port, console.log(`app listening on port ${port}`));
+const startServer = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is  listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
