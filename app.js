38fdc07cf8abeac5d5e667a114111ed7e07142af
iamrithu <ritimahesh29@gmail.com>
2022-05-09 22:56:33 +0530

    --

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const tasks = require("./routes/task");
 const connectDB = require("./db/connect");
 const notFound = require("./middleware/not-found");
